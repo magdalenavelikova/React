2952d5a1a4c56a3395581e4e64aebc5737527d9f
MLVelikova <magibox@yahoo.com>
2023-08-16 16:36:01 +0300

    conditional functions

--- react-component-demo/src/components/Counter.js
+++ react-component-demo/src/components/Counter.js
@@ -2,14 +2,24 @@ import { useState } from "react";
 
 export default function Counter(props) {
   const [counter, setCounter] = useState(0);
-  
-  const onButtonClick=(e)=>{
-    setCounter(state=>state+1);
+
+  const onButtonClickDecrement = (e) => {
+    setCounter((state) => state + 1);
   };
+  const onButtonClickIncrement = (e) => {
+    setCounter((state) => state + -1);
+  };
+  const onButtonClickClear = (e) => {
+    setCounter(0);
+  };
+
   return (
     <div>
       <h3>Counter: {counter}</h3>
-      <button onClick={onButtonClick}>+</button>
+      <button onClick={onButtonClickDecrement}>+</button>
+      <button onClick={onButtonClickIncrement}>-</button>
+      <button onClick={onButtonClickClear}>Clear</button>
+
     </div>
   );
 }
