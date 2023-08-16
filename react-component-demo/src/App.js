767eb86324c996f3258c874cab5c8d99626b21d7
MLVelikova <magibox@yahoo.com>
2023-08-16 14:44:44 +0300

    components with props

--- react-component-demo/src/App.js
+++ react-component-demo/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import MovieList from "./components/MovieList";
 
 function App() {
+  const movies = [
+    { title: "Test1", year: "2000", cast: ["Me ", "You "] },
+    { title: "Test2", year: "2000", cast: ["Me ", "You "] },
+    { title: "Test3", year: "2000", cast: ["Me ", "You "] }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieList movies={movies} />
     </div>
   );
 }
---
+++ react-component-demo/src/components/Movie.js
@@ -0,0 +1,15 @@
+const Movie = (props) =>{
+    return (
+    <article>
+        <h2>{props.title}</h2> 
+        <p>Year: {props.year}</p>
+        <ul> Cast
+        <li>{props.cast[0]}</li>
+        <li>{props.cast[1]}</li>
+        </ul>
+        
+        
+         
+         </article>);
+};
+export default Movie;
