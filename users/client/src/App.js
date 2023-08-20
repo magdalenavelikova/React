bb50a35b83b6564133707de494bf22f17d22084c
MLVelikova <magibox@yahoo.com>
2023-08-20 10:19:11 +0300

    remove async

--- users/client/src/App.js
+++ users/client/src/App.js
@@ -1,19 +1,22 @@
-import * as userService from './services/userService';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import { Fragment, useEffect } from 'react';
-import Search from './components/Search';
-import UserList from './components/UserList';
-import NewUser from './components/NewUser';
-import './App.css';
+import * as userService from "./services/userService";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Fragment, useEffect } from "react";
+import Search from "./components/Search";
+import UserList from "./components/UserList";
+import NewUser from "./components/NewUser";
+import "./App.css";
 function App() {
- useEffect(()=>{
-  async function getUsers(){
-     const users=await userService.getAll();
-  }
-  getUsers();
- }
-  ,[]);
+  useEffect(() => {
+    userService
+      .getAll()
+      .then((users) => {
+        console.log(users);
+      })
+      .catch((err) => {
+        console.log("Error: " + err);
+      });
+  }, []);
   return (
     //<></> without import Fragment;
     <Fragment>
@@ -22,9 +25,8 @@ function App() {
       <main className='main'>
         <section className='card users-container'>
           <Search />
-          <UserList/>
-          <NewUser/>
-
+          <UserList />
+          <NewUser />
         </section>
       </main>
 
