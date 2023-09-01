abbe88eca95f17481699ba3e8259b3c9356c3f5c
MLVelikova <magibox@yahoo.com>
2023-09-01 14:56:30 +0300

    Route Guard example 1

--- games-play/client/src/components/Common/RouteGuard.js
+++ games-play/client/src/components/Common/RouteGuard.js
@@ -1,12 +1,13 @@
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../../contexts/AuthContext";
 
 export const RouteGuard = ({ children }) => {
   const { isAuthenticated } = useAuthContext();
-  const navigate = useNavigate();
+ 
 
   if (!isAuthenticated) {
-    navigate("/login");
+  return < Navigate to = "/login"/>;
+ 
   }
   return <>{children}</>;
 };
